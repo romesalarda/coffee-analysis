e28610ea5f76781cd532252447776da965bffd15
Rogerium2102 <MatthewR2110@outlook.com>
2026-03-25 11:11:29 +0000

    Added weighting based on species, uniformity, flavor, and aroma

--- Weighting.py
+++ Weighting.py
@@ -1,3 +1,19 @@
+import pandas as pd
+import numpy as np
 
-def weightColumnValue(df, hgf):
-    print("Hello")
+def get_scoring(df):
+    total_score = pd.Series(0, index=df.index)
+    total_score += np.where(df['species'] == 'Arabica', 1.25, 1.0)
+    total_score += np.where(df['uniformity'] >= 8, df['uniformity'] * 1.75, df['uniformity'].fillna(0))
+    total_score += np.where(df['flavor'] >= 7.5, df['flavor'] * 1.5, df['flavor'].fillna(0))
+    total_score += np.where(df['aroma'] >= 7, df['aroma'] * 1.25, df['aroma'].fillna(0))
+    cols_to_exclude = ['species', 'uniformity', 'flavor', 'aroma']
+    other_nums = df.drop(columns = cols_to_exclude).select_dtypes(include = [np.number])
+    total_score += other_nums.sum(axis=1)
+    return total_score
+
+df = pd.read_csv("data/simplified_coffee_ratings.csv")
+df['final_score'] = get_scoring(df)
+
+for score in df['final_score']:
+    print(score)
