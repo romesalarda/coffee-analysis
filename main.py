2b6b69ee0ae2611db90de72a59b5f02cdd9fb424
Hathem Trabelsi <hathem23.trabelsi@gmail.com>
2026-03-26 14:21:51 +0000

    added license and expanded report body

--- main.py
+++ main.py
@@ -50,4 +50,23 @@ if __name__ == "__main__":
     # scatter_graph_flavor.show()
     # scatter_graph_flavor.save_graph("top_countries_flavor.png")
 
-    generate_report(f"The best country to buy coffee from is <b>{top_countries[0]}</b>.\nThis answer was reached by taking an <b> average </b> of each countries local suppliers - weighted according to customer preferences.", [bar_graph.WORKING_DIR + "/top_countries_bar.png"], "report.pdf")
+    report_body = f"""
+                      This report outlines country with the best bean-buying prospects.\n <br>
+                      These results are based on data published by the Coffee Quality Database and compiled by data scientist James LeDoux available <a href=https://github.com/jldbc/coffee-quality-database>here</a>. \n
+                      The data contains reviews of around 1300 coffee beans from across the world.\n
+                      
+                      <h3> Methodology </h3>\n
+                      First the data is parsed and filtered to remove suppliers with key missing or invalid values in key data fields. This is done so that only real data is used in the process and not any kind of guessing or interpolation between similar suppliers.\n <br>
+                      Next we removed countries with less than {3} suppliers from the dataset. This was done as these kinds of countries are prone to generating biased data - and since they are less valuable as trading partners.
+                      And finally, the last filter we apply is to ensure that the coffee uses a <i>Washed/Wet</i> processing method. This is a requirement identified as necessary to ensure consistency across coffee outlets. <br> <br>
+                      A coffee's quality is described in the dataset as a list of scores in the following categories: <br><i>(aroma,flavor,aftertaste,acidity,body,balance,uniformity,clean_cup,sweetness,cupper_points,moisture)</i><br>
+                      We apply a weighted sum to each supplier across each of these categories such that for supplier i: <br>
+                      <i>score<sub>i</sub> = (w<sub>a</sub> . aroma<sub>i</sub>) + (w<sub>f</sub> . flavour<sub>i</sub>) + (w<sub>u</sub> . uniformity<sub>i</sub>) + Σ<sub>other</sub> (w<sub>other</sub> + other<sub>i</sub>) <br>
+                      and w<sub>a</sub>=<b>{1.0}</b>, w<sub>f</sub>=<b>{1.0}</b>, w<sub>u</sub>=<b>{1.0}</b>, w<sub>other</sub>=<b>{1.0}</b></i> <br> <br>
+
+                      At this point an average of each countries to {20} top suppliers is taken. These are the values used in determining which country is the best to trade with.
+                      <h3> Results </h3>\n
+                   """
+
+
+    generate_report(report_body, [bar_graph.WORKING_DIR + "/top_countries_bar.png"], "report.pdf")
